54f1e965ac382692f0ae9dd8d1c7cbe49345824c
rara26246 <ST10505922@myemeris.edu.za>
2026-02-25 14:18:25 +0200

    Final Submission

---
+++ src/stopandgo/STOPANDGO.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package stopandgo;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class STOPANDGO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+so
